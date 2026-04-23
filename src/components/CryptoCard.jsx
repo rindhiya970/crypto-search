72a8e7d70301ccfd30b52b559ff55b04e687654a
rindhiya970 <rindhiya731@gmail.com>
2026-04-23 23:09:42 +0530

    Add flash animation for price changes in CryptoCard component

--- src/components/CryptoCard.jsx
+++ src/components/CryptoCard.jsx
@@ -1,12 +1,24 @@
 import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 import "./CryptoCard.css";
 
 export default function CryptoCard({ coin }) {
   const priceChange = coin.price_change_percentage_24h;
-  const isPositive = priceChange >= 0;
+  const isPositive  = priceChange >= 0;
+  const prevPrice   = useRef(coin.current_price);
+  const [flash, setFlash] = useState("");
+
+  useEffect(() => {
+    if (coin.current_price !== prevPrice.current) {
+      setFlash(coin.current_price > prevPrice.current ? "flash-up" : "flash-down");
+      const t = setTimeout(() => setFlash(""), 1000);
+      prevPrice.current = coin.current_price;
+      return () => clearTimeout(t);
+    }
+  }, [coin.current_price]);
 
   return (
-    <Link to={`/details/${coin.id}`} className="crypto-card">
+    <Link to={`/details/${coin.id}`} className={`crypto-card ${flash}`}>
       <div className="card-header">
         <img src={coin.image} alt={coin.name} width={36} height={36} />
         <div>
