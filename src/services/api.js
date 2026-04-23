8c91ae044ae0408738fe7804bc48140923561afe
rindhiya970 <rindhiya731@gmail.com>
2026-04-23 22:50:49 +0530

    Add API service functions for fetching cryptocurrency data

---
+++ src/services/api.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.coingecko.com/api/v3";
+
+// Fetch top coins by market cap
+export const getTopCoins = async (limit = 20) => {
+  const { data } = await axios.get(`${BASE_URL}/coins/markets`, {
+    params: {
+      vs_currency: "usd",
+      order: "market_cap_desc",
+      per_page: limit,
+      page: 1,
+      sparkline: false,
+    },
+  });
+  return data;
+};
+
+// Search coins by query
+export const searchCoins = async (query) => {
+  const { data } = await axios.get(`${BASE_URL}/search`, {
+    params: { query },
+  });
+  return data.coins;
+};
+
+// Get detailed info for a single coin
+export const getCoinDetails = async (id) => {
+  const { data } = await axios.get(`${BASE_URL}/coins/${id}`, {
+    params: { localization: false, tickers: false, community_data: false },
+  });
+  return data;
+};
+
+// Get historical price chart data (7 days)
+export const getCoinChart = async (id, days = 7) => {
+  const { data } = await axios.get(`${BASE_URL}/coins/${id}/market_chart`, {
+    params: { vs_currency: "usd", days },
+  });
+  return data.prices; // [[timestamp, price], ...]
+};
