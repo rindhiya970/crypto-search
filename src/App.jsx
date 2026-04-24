35c61ef36588d72b44896001c9791238715bc2d1
rindhiya970 <rindhiya731@gmail.com>
2026-04-24 22:45:15 +0530

    Wrap App component with FavoritesProvider and add Watchlist route

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import Details from "./pages/Details";
+import Watchlist from "./pages/Watchlist";
+import { FavoritesProvider } from "./context/FavoritesContext";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/details/:id" element={<Details />} />
-      </Routes>
-    </BrowserRouter>
+    <FavoritesProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/watchlist" element={<Watchlist />} />
+        </Routes>
+      </BrowserRouter>
+    </FavoritesProvider>
   );
 }
